66355962cafd6ae3590b0b6f56466ba57f2b58a9
Aaron Brightman <aaronbrightman1@gmail.com>
2017-06-18 10:44:29 -0400

    Error middleware for errors

--- app.js
+++ app.js
@@ -15,5 +15,14 @@ app.use(bodyParser.json());
 
 routes(app);
 
+//Error middleware should be after the routes (routing methods)
+//// err variable represents an err in the previous route
+//===> This middleware will only be called if next is called in the previous method
+//      ie .catch(next)
+app.use((err,req,res,next)=>{
+    res.status(422).send({error:err._message});
+});
+
+
 
 module.exports = app;
--- controllers/driver_controller.js
+++ controllers/driver_controller.js
@@ -5,22 +5,15 @@ greeting : function(req,res){
     res.send({hi:'there'});
 },
 
-create : function(req,res){
+// The third parameter of the request 'handlers' are all 'next'
+create : function(req,res,next){
    
-    // // var newDriver = new Driver(req.body);
-    // var newDriver = new Driver({
-    //     email : req.body.email
-    // });
-
-    // newDriver.save()
-    // .then(()=>{
-    //     res.send(newDriver);
-    // })
-
+    
     Driver.create(req.body)
     .then(NewDriver=>{
         res.send(NewDriver);
-    });
+    })
+    .catch(next);
 
 }
 
